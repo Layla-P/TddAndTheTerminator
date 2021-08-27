c490e0411a1b00951008a3a33d5e81b9db7fa9c2
layla-p <layla.porter@gmail.com>
2021-08-27 12:19:34 +0100

    updatedto .net 5

--- TerminatorProcess/Tests/SubjectAnalyserTests.cs
+++ TerminatorProcess/Tests/SubjectAnalyserTests.cs
@@ -1,26 +1,30 @@
+using FluentAssertions;
 using NUnit.Framework;
+using TerminatorProcess.Models;
 using TerminatorProcess.Services;
 
 namespace TerminatorProcess.Tests
 {
-    [TestFixture]
-    public class SubjectAnalyserTests
-    {
-        
-        private ISubjectAnalyser _sut;
-        
-        
-        [SetUp]
-        public void Setup()
-        {
-            
-        } 
-        
-        
-        //** Scan subjects and determine if they require further investigation **
-        // - Scan subjects and see if they are of interest
-        
-        
-        
-    }
+	[TestFixture]
+	public class SubjectAnalyserTests
+	{
+
+		private ISubjectAnalyser _sut;
+
+
+		[SetUp]
+		public void Setup()
+		{
+
+
+		}
+
+
+
+		//** Scan subjects and determine if they require further investigation **
+		// - Scan subjects and see if they are of interest
+
+
+
+	}
 }
