bb48eb4cc949b34e96b3469cd83b989a3f49403b
layla-p <layla.porter@gmail.com>
2022-04-27 14:26:35 +0100

    completed version upgraded to .net 6

---
+++ TerminatorProcess/Services/SubjectAnalyser.cs
@@ -0,0 +1,17 @@
+using TerminatorProcess.Models;
+
+namespace TerminatorProcess.Services
+{
+    public class SubjectAnalyser :ISubjectAnalyser
+    {
+        public bool InvestigateFurther(ISubject subject)
+        {
+            if (subject.SubjectName == "woman")
+                return true;
+            else if (subject.SubjectName == "boy")
+                return true;
+            else
+                return false;
+        }
+    }
+}
