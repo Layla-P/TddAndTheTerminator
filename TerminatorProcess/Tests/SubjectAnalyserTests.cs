c3cfb823ee38003325ac284bf5fb9100ff88347f
layla-p <layla.porter@gmail.com>
2022-04-27 14:33:46 +0100

    .net 6 start point

--- TerminatorProcess/Tests/SubjectAnalyserTests.cs
+++ TerminatorProcess/Tests/SubjectAnalyserTests.cs
@@ -15,46 +15,14 @@ namespace TerminatorProcess.Tests
         [SetUp]
         public void Setup()
         {
-            _sut = new SubjectAnalyser();
+           
         } 
         
         
         //** Scan subjects and determine if they require further investigation **
         // - Scan subjects and see if they are of interest
 
-        
-        [TestCase("woman")]
-        [TestCase("boy")]
-        public void TerminatorShould_Determine_ToInvestigateFurther(string subjectName)
-        {
-            //arrange
-            var subject = new Subject
-            {
-                SubjectName = subjectName
-            };
-            //Act
-            var result = _sut.InvestigateFurther(subject);
-            //Assert
-
-            result.Should().BeTrue();
-
-        }
-
-        [Test]
-        public void TerminatorShould_DetermineNot_ToInvestigateFurther()
-        {
-            //arrange
-            var subject = new Subject
-            {
-                SubjectName = "t1000"
-            };
-            //Act
-            var result = _sut.InvestigateFurther(subject);
-            //Assert
-
-            result.Should().BeFalse();
-        }
-        
+       
         
     }
 }
